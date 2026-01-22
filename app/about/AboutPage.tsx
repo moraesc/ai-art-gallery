b8458809863badd1c7bd8a26cafb3db2b9639ff9
moraesc <cmoraes1955@gmail.com>
2026-01-21 19:23:39 -0800

    Add header

--- app/about/AboutPage.tsx
+++ app/about/AboutPage.tsx
@@ -4,6 +4,7 @@ export const AboutPage = () => {
     return (
       <div className='relative flex flex-col lg:flex-row justify-center items-center mt-24 md:mt-48'>
         <div className='flex flex-col justify-between items-center'>
+          <h1 className='text-white text-[32px] md:text-[48px] font-[Open Sans] font-bold mb-8'>About the AI Art Gallery</h1>
           <p className='text-white text-[18px] md:text-[20px] w-full px-2 md:w-[520px] font-[Open Sans] font-thin'>
             The AI Art Gallery was established in 2022 with the objective of showcasing the creative potential of AI art tools such as Midjourney and DALL•E.
           </p>
